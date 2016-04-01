bd59021e2304a4b390abf02c0edc462d706269df
sbssmadhav <madhav@alum.mit.edu>
2016-04-01 13:51:45 -0500

    first commit concert concierge

---
+++ ConcertConcierge/Concert.swift
@@ -0,0 +1,9 @@
+//
+//  Concert.swift
+//  ConcertConcierge
+//
+//  Created by Srimadh Bhagavatam, Madhav on 2/28/16.
+//  Copyright © 2016 Srimadh Bhagavatam, Madhav. All rights reserved.
+//
+
+import Foundation
---
+++ ConcertViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConcertViewCell.swift
+//  ConcertConcierge
+//
+//  Created by Srimadh Bhagavatam, Madhav on 2/28/16.
+//  Copyright © 2016 Srimadh Bhagavatam, Madhav. All rights reserved.
+//
+
+import UIKit
+
+class ConcertViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
